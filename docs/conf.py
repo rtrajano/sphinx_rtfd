72e5e39d21478dce95df9e49c1791b8f4de42a78
Rigel Trajano <whichone@gmail.com>
2014-05-13 23:41:13 +0000

    added automod directive

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = ['.rst', '.py']
+source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'

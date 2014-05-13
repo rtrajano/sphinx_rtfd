fb1fa8fc883513b7da6a848dd6aab9bf2aa1cfac
Rigel Trajano <whichone@gmail.com>
2014-05-13 23:26:19 +0000

    added docstrings

--- sphinx_rtfd/sphinx_rtfd.py
+++ sphinx_rtfd/sphinx_rtfd.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Module level docstring for the Greeter class"""
+
+class Greeter():
+    """A greeter class Greets people"""
+
+    def __init__(self, msg, name):
+        """Init func
+
+        args:
+          msg (str): a msg for the greeting
+          name (str): someone to greet
+
+        """
+        self.msg = msg
+        self.name = name
+
+    def greet(self):
+        """Greets a user.
+
+        .. Example::
+        >>> my_greeter = Greeter('Hello', 'PUG')
+        >>> my_greeter.greet()
+        'Hello, PUG!!'
+        """
+        return "{}, {}!".format(self.msg, self.name)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
